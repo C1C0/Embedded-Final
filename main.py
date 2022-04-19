d61b402ed3628f99c1342f890d71388df3f7a855
C1C0 <kpskola1002@gmail.com>
2022-04-19 13:11:42 +0200

    fix

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import display
 import threading
 import multiprocessing
 
-import os
-os.environ["SDL_VIDEO_CENTERED"] = "1"
-
 if __name__ == "__main__":
     THWebhooks = multiprocessing.Process(target=webhooks.setup)
     THWebhooks.start()
